7945ed0d75bc2798a483b80f4ba36e04268a57af
Đỗ Văn Hải <haidv28062004@gmail.com>
2025-08-15 14:25:27 +0700

    sửa lại thông số flex cho book detail

--- src/features/books/sections/detail/DetailBodyThreeCols.tsx
+++ src/features/books/sections/detail/DetailBodyThreeCols.tsx
@@ -13,17 +13,17 @@ const DetailBodyThreeCols: React.FC<DetailBodyThreeColsProps> = ({ book }) => {
     <div className="w-full px-4 py-8">
       <div className="flex gap-4 w-full justify-center">
         {/* Left Gallery */}
-        <div className="flex-1 sticky top-4 self-start">
+        <div className="flex-2 sticky top-4 self-start">
           <DetailLeftGallery book={book} />
         </div>
         
         {/* Center Info */}
-        <div className="flex-2">
+        <div className="flex-3">
           <DetailCenterInfo book={book} />
         </div>
         
         {/* Right Purchase */}
-        <div className="flex-1 sticky top-4 self-start">
+        <div className="flex-2 sticky top-4 self-start">
           <DetailRightPurchase book={book} />
         </div>
       </div>
